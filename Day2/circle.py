45dbf1a2cff7f3eceb2f0b0cb93dba7b8c18505d
santiagomonl <santiago.mon.1996@gmail.com>
2024-11-13 20:29:21 +0200

    Add files via upload

---
+++ Day2/circle.py
@@ -0,0 +1,6 @@
+r=float(input('Enter the radius of a circle: '))
+pi = 3.14159265359
+area = pi * r**2
+circumference = 2 * pi * r
+print("Area:", area)
+print("Circumference:", circumference)
---
+++ Day2/rectangle.py
@@ -0,0 +1,7 @@
+length=float(input('Enter the legth of a rectangle: '))
+width=float(input('Enter the Width of a rectangle: '))
+area = width * length
+circumference = 2 * (length + width)
+print("Area:", area)
+print("Circumference:", circumference)
+
